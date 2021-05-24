e1c8af5e1ed9ded82bde37f2474306005c7a0f5a
Klaudio Milankovic <klaudio.milankovic@gmail.com>
2021-05-24 23:19:37 +0200

    Remove SCSS

--- build/frontend.js
+++ build/frontend.js
@@ -2,38 +2,47 @@
 
 document.addEventListener("DOMContentLoaded", function () {
   // code...
-  blogSlider = tns({
-    container: ".js-blog-slider",
-    mode: "gallery",
-    items: 1,
-    slideBy: 1,
-    mouseDrag: true,
-    controlsContainer: '.blog-slider__controls',
-    prevButton: '.blog-slider-prev',
-    nextButton: '.blog-slider-next',
-    nav: false
-  }); // Posts slider
+  var jsBlogSlider = document.querySelector('.js-blog-slider');
 
-  postSlider = tns({
-    container: ".js-post-slider",
-    items: 1,
-    mouseDrag: true,
-    slideBy: 1,
-    gutter: 33,
-    controlsContainer: ".post-slider__navigation",
-    prevButton: ".post-slider-prev",
-    nextButton: ".post-slider-next",
-    nav: false,
-    fixedWidth: 256,
-    responsive: {
-      480: {
-        items: 2,
-        fixedWidth: false
-      },
-      1024: {
-        items: 3
+  if (document.body.contains(jsBlogSlider)) {
+    blogSlider = tns({
+      container: ".js-blog-slider",
+      mode: "gallery",
+      items: 1,
+      slideBy: 1,
+      mouseDrag: true,
+      controlsContainer: ".blog-slider__controls",
+      prevButton: ".blog-slider-prev",
+      nextButton: ".blog-slider-next",
+      nav: false
+    });
+  } // Posts slider
+
+
+  var postsSlider = document.querySelector('.js-post-slider');
+
+  if (document.body.contains(postsSlider)) {
+    postSlider = tns({
+      container: ".js-post-slider",
+      items: 1,
+      mouseDrag: true,
+      slideBy: 1,
+      gutter: 33,
+      controlsContainer: ".post-slider__navigation",
+      prevButton: ".post-slider-prev",
+      nextButton: ".post-slider-next",
+      nav: false,
+      fixedWidth: 256,
+      responsive: {
+        480: {
+          items: 2,
+          fixedWidth: false
+        },
+        1024: {
+          items: 3
+        }
       }
-    }
-  });
+    });
+  }
 });
 "use strict";
